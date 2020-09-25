f2d5b4a0b960722ddd9cf29407f7b821f008fe5d
Olivier AJ <54745057+Olivier9925@users.noreply.github.com>
2020-09-25 10:13:25 +0000

    Ajout des fichier d'initialisation

---
+++ app/MyAbeilles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MyAbeilles = () => {
+    return (
+        <>
+            <Text>MyAbeilles coucou</Text>
+        </>
+    );
+}
+
+export default MyAbeilles;
